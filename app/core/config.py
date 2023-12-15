7bd94d625b0f66b8742df0eb2a356cee84478b75
kudakwashe chris chipangura <49289670+kudah99@users.noreply.github.com>
2023-12-15 21:55:48 +0200

    Changed database from sqlite to postgres

--- app/core/config.py
+++ app/core/config.py
@@ -15,10 +15,27 @@ class Configs(BaseSettings):
         os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     )
 
-    DATABASE_URI = "{db_engine}:///{path}/sqlite.db".format(
-        db_engine=DB_ENGINE,
-        path=PROJECT_ROOT
+    # database
+    DB: str = os.getenv("DB", "postgresql")
+    DB_USER: str = os.getenv("DB_USER")
+    DB_PASSWORD: str = os.getenv("DB_PASSWORD")
+    DB_HOST: str = os.getenv("DB_HOST")
+    DB_PORT: str = os.getenv("DB_PORT", "5432")
+    DB_ENGINE: str = "postgresql"
+
+    DATABASE_URI_FORMAT: str = (
+        "{db_engine}://{user}:{password}@{host}:{port}/{database}"
     )
+
+    DATABASE_URI = "{db_engine}://{user}:{password}@{host}:{port}/{database}".format(
+        db_engine=DB_ENGINE,
+        user=DB_USER,
+        password=DB_PASSWORD,
+        host=DB_HOST,
+        port=DB_PORT,
+        database="ecommerce",
+    )
+
 
     # find query
     PAGE = 1
